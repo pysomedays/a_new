04c135427ab0a51771f57b53612078bdaa319fa4
pysomedays <pysomedays@163.com>
2017-10-21 23:02:46 +0800

    鎻愪氦鏂囦欢20171008_txt2html

--- git_commit.bat
+++ git_commit.bat
@@ -1,6 +1,8 @@
 git init
 git config --global user.name "pysomedays"
 git config --global user.email "pysomedays@163.com"
+git config --global i18n.commitencoding utf-8
+git config --global i18n.logoutputencoding gbk
 pause
 git add .
 git commit -m "提交文件20171008_txt2html"
