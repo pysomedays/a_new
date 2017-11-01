367313fb78eaaba18aa7c85adc3631628ac3c595
pysomedays <pysomedays@163.com>
2017-11-02 02:05:19 +0800

    change files

--- git_commit.bat
+++ git_commit.bat
@@ -3,9 +3,9 @@ git config --global user.name "pysomedays"
 git config --global user.email "pysomedays@163.com"
 git config --global i18n.commitencoding utf-8
 git config --global i18n.logoutputencoding gbk
-pause
-git add .
-git commit -m "提交文件20171008_txt2html"
+
+git add AutoMessage
+git commit -m "change AutoMessage folder: adding some files"
 git remote add -f origin https://github.com/pysomedays/a_new.git
 git push -u origin master
 pause
